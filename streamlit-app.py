d8c453d8b4edb4f14cad8344ccf8f2265f802895
Qoid Rif'at <125227459+qoidrifat@users.noreply.github.com>
2024-11-28 08:46:46 +0700

    Add files via upload

--- streamlit-app.py
+++ streamlit-app.py
@@ -4,6 +4,7 @@ import pandas as pd
 import numpy as np
 import joblib
 import matplotlib.pyplot as plt
+import seaborn as seaborn
 from sklearn.preprocessing import StandardScaler
 import skfuzzy as fuzz
 
